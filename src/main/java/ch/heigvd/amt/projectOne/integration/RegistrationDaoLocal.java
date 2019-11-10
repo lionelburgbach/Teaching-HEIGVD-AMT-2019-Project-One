82112816f2d5aa044795e9e704112e06a2b03a6a
guitch94 <guillaume.blanco@heig-vd.ch>
2019-11-10 17:57:41 +0100

    commentaire pour RegDaoLocal et TrailDaoLocal

--- src/main/java/ch/heigvd/amt/projectOne/integration/RegistrationDaoLocal.java
+++ src/main/java/ch/heigvd/amt/projectOne/integration/RegistrationDaoLocal.java
@@ -8,16 +8,71 @@ import java.util.List;
 @Local
 public interface RegistrationDaoLocal {
 
+    /**
+     *
+     * @param idUser is the id of the user who want to regitrate in the trail
+     * @param idTrail is the id of the trail where the user want to registrate
+     * @return the regitration of the user in the trail
+     */
     Registration registration(long idUser, long idTrail);
 
+    /**
+     *
+     * @param idUser ID that we use to check if the registration contain the user
+     * @return a list of all registration of a user
+     */
     List<Registration> allRegUser(long idUser);
+
+    /**
+     *
+     * @param idUser ID that we use to check if the registration contain the user
+     * @return the number of registration who contain the user
+     */
     int getNumberOfRegsUser(long idUser);
+
+    /**
+     *
+     * @param idUser ID that we use to check if the registration contain the user
+     * @param currentPage who represent the page where is the client actually
+     * @param elementPerPage who represent the number of registration that we want to retrieve
+     * @return a list who contain the number of elementPerPage registrations who contain the user
+     */
     List<Registration> allRegUserPagination(long idUser, int currentPage, int elementPerPage);
 
+    /**
+     *
+     * @param idTrail ID that we use to check if the registration contain the trail
+     * @return a list of all registration at a trail
+     */
     List<Registration> allRegTrail(long idTrail);
+
+    /**
+     *
+     * @param idTrail ID that we use to check if the registration contain the trail
+     * @return the number of registration who contain the trail
+     */
     int getNumberOfRegsTrail(long idTrail);
+
+    /**
+     *
+     * @param idTrail ID that we use to check if the registration contain the trail
+     * @param currentPage who represent the page where is the client actually
+     * @param elementPerPage who represent the number of registration that we want to retrieve
+     * @return a list who contain the number of elementPerPage registrations who contain the trail
+     */
     List<Registration> allRegTrailPagination(long idTrail, int currentPage, int elementPerPage);
 
+    /**
+     *
+     * @param reg the registration that we want to add in the DB
+     * @return the ID of the registration
+     */
     long addReg(Registration reg);
+
+    /**
+     *
+     * @param id the ID of the registration that we want to delete
+     * @return a boolean who indicate if the suppression passed
+     */
     boolean deleteReg(long id);
 }
--- src/main/java/ch/heigvd/amt/projectOne/integration/TrailDaoLocal.java
+++ src/main/java/ch/heigvd/amt/projectOne/integration/TrailDaoLocal.java
@@ -8,17 +8,74 @@ import java.util.List;
 @Local
 public interface TrailDaoLocal {
 
+    /**
+     *
+     * @param id of the trail that we want to retrieve
+     * @return the trail corresponding to the id
+     */
     Trail trail(long id);
 
+    /**
+     *
+     * @return a list who contain all the trails
+     */
     List<Trail> allTrail();
+
+    /**
+     *
+     * @return the number of all trails
+     */
     int getNumberOfTrails();
+
+    /**
+     *
+     * @param currentPage who represent the page where is the client actually
+     * @param elementPerPage who represent the number of trail that we want to retrieve
+     * @return a list of trail who contain the number of elementPerPage trail for the currentPage
+     */
     List<Trail> allTrailPagination(int currentPage, int elementPerPage);
 
+    /**
+     *
+     * @param idUser represent the id of the user what we use to check the registration (if he is in)
+     * @return all the trail who not contain a registration with this user
+     */
     List<Trail> allTrailToComeWithNoReg(long idUser);
+
+    /**
+     *
+     * @param idUser represent the id of the user what we use to check the registration (if he is in)
+     * @return the number of trail who not contain a registration with this user
+     */
     int getNumberOfTrailsToComeWithNoReg(long idUser);
+
+    /**
+     *
+     * @param idUser represent the id of the user what we use to check the registration (if he is in)
+     * @param currentPage who represent the page where is the client actually
+     * @param elementPerPage who represent the number of trail that we want to retrieve
+     * @return a list of trail who contain the number of elementPerPage trail for the currentPage and that we have no registration with the user
+     */
     List<Trail> allTrailToComeWithNoRegPagination(long idUser, int currentPage, int elementPerPage);
 
+    /**
+     *
+     * @param trail that we want to add to the DB
+     * @return the id of the trail
+     */
     long addTrail(Trail trail);
+
+    /**
+     *
+     * @param trail that we want to modify (need that the ID is already set in the trail)
+     * @return a boolean who indicate if the change passed
+     */
     boolean updateTrail(Trail trail);
+
+    /**
+     *
+     * @param id of the trail that we want to delete
+     * @return a boolean who indicate if the suppression passed
+     */
     boolean deleteTrail(long id);
 }
